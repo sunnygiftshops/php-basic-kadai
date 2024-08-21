0b598bf8ad3de8266aacffd9192e29a85a6ce28c
sunnygiftshops <sunnygiftshops@gmail.com>
2024-08-21 18:20:55 +0900

    課題　テストの平均点を計算しよう

---
+++ kadai_005/score.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>テストの平均点を計算しよう</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 変数に値を代入する
+         average_score = ( 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60 )/10;
+         
+         // 変数の中身を出力する
+         echo average_score;
+         ?>
+     </p>
+ </body>
+ 
+ </html>
